7fd6597ab3bdb05ddd775b9f39f3b9756e83c5d3
Sean Fallon <sfallon7@gmail.com>
2021-11-17 00:32:26 -0500

    finished login user story and testing. started listing story

--- src/main/java/com/fallon/sneakerapp/dtos/LoginDTO.java
+++ src/main/java/com/fallon/sneakerapp/dtos/LoginDTO.java
@@ -3,4 +3,27 @@ package com.fallon.sneakerapp.dtos;
 public class LoginDTO {
     //TODO
     //DTO
+    String username;
+    String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LoginDTO() {
+        super();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
