e2c5cac370dd7b36dbecc31dc78ad60a545a5a1f
James Fallon <jfallon432@gmail.com>
2021-12-06 12:31:06 -0500

    deleted unfound package in User controller

--- src/main/java/com/fallon/sneakerapp/controllers/UserController.java
+++ src/main/java/com/fallon/sneakerapp/controllers/UserController.java
@@ -9,10 +9,8 @@ import com.fallon.sneakerapp.pojos.User;
 import com.fallon.sneakerapp.services.UserServices;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
