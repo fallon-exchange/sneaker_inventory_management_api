7cc60a7e0b6db7dffa096427dd5a26f975e22469
James Fallon <jfallon432@gmail.com>
2021-12-06 16:56:03 -0500

    adding test

--- src/main/java/com/fallon/sneakerapp/servlets/DispatcherServlet.java
+++ src/main/java/com/fallon/sneakerapp/servlets/DispatcherServlet.java
@@ -70,7 +70,14 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String target = parseURI(request);
+        objectMapper = new ObjectMapper();
 
+        switch (target) {
+            case "test":
+                response.getWriter().println("<h1>Sneaker App Deployed<h1>");
+                break;
+        }
     }
 
     @Override
